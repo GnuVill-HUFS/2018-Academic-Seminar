1876c68dfe5c8924615d274000246e80a0a5ef9c
HeoSeungeun <38011960+HeoSeungeun@users.noreply.github.com>
2018-11-13 19:04:16 +0900

    B서버에서 아두이노
    상태 코드 호출, 예외처리 딕셔너리 post

--- Bserver/app.py
+++ Bserver/app.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+import requests
+from templates import Connector
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+@app.route('/B_rasp ')
+def B_rasp(direction_code):
+    try:
+        Connector.serial_connection(direction_code)
+    except:
+        HaveError = {'word':'Error!!!!', 'code_num': 555}
+        res = requests.post('CheckingURL', HaveError)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
