0c2c54b52dd33422772ea2e436691786dd1102f0
jenny6585 <39253236+jenny6585@users.noreply.github.com>
2018-11-13 19:05:19 +0900

    A서버 전체
    우영.html(수정 필요)
    URL 수정 필요

--- Aserver/app.py
+++ Aserver/app.py
@@ -1,33 +1,20 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template
+import requests
 
 app = Flask(__name__)
 
-class CodeError(Exception):
-    def __init__(self, a):
-        self.a = int(555)
-
-    value = voice_assist(STATUS)
-
-    if value != 303 or 309 or 306 or 321:
-        raise CodeError(555)
-
-#예외처리
-def if_it_doesnt_work(direction_code):
+@app.route('/aaron', methods=['POST'])
+def A_rasp(aaron_num, aaron_str):
     try:
-        if direction_code == 303 or 309 or 306 or 321:
-            return direction_code
-    except: CodeError
+        name = {'word':aaron_str, 'code_num': aaron_num}
+        res = requests.post('URL', data = aaron_num)
+    except:
+        HaveError = {'word':'Error!!!!', 'code_num': 555}
+        return render_template(우영.html, names=HaveError)
 
+@app.route('/checking',methods=['POST'])
+def Check(back):
+    return render_template(우영.html, names=back)
 
-code_list=[]
-
-@app.route('/direction/<int: direction_code> ',methods=['GET, POST'])
-def A_rasp(direction_code):
-    if request.method =='POST':
-        code_list[0] = direction_code
-        return code_list, render_template(우영.html, name=direction_code)
-
-def B_rasp():
-    if request.method =='POST':
-        a = int (code_list[0])
-        return a
+if __name__ ==  '__main__':
+    app.run()
