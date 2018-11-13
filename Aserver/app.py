09ad2c64d8fe72bb47a7b31095dc6c3ef5526717
jenny6585 <39253236+jenny6585@users.noreply.github.com>
2018-11-13 15:19:07 +0900

    서버파트 전체
    예외처리, A서버 B 서버 부분

--- Aserver/app.py
+++ Aserver/app.py
@@ -1,13 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,request
+
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome!"
+class CodeError(Exception):
+    def __init__(self, a):
+        self.a = int(555)
 
-@app.route("/status")
-def example():
-    return render_template('main.html')
+    value = voice_assist(STATUS)
 
-if __name__ == "__main__":
-    app.run()
+    if value != 303 or 309 or 306 or 321:
+        raise CodeError(555)
+
+#예외처리
+def if_it_doesnt_work(direction_code):
+    try:
+        if direction_code == 303 or 309 or 306 or 321:
+            return direction_code
+    except: CodeError
+
+
+code_list=[]
+
+@app.route('/direction/<int: direction_code> ',methods=['GET, POST'])
+def A_rasp(direction_code):
+    if request.method =='POST':
+        code_list[0] = direction_code
+        return code_list, render_template(우영.html, name=direction_code)
+
+def B_rasp():
+    if request.method =='POST':
+        a = int (code_list[0])
+        return a
