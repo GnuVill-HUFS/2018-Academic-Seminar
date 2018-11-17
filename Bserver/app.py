069a31b786f09817a62a0dcd397abfeea6501aa5
HeoSeungeun <38011960+HeoSeungeun@users.noreply.github.com>
2018-11-18 01:09:04 +0900

    json으로 dictionary값 받아옴, pyserial에 값 전달 수정
    pyserial 함수 호출하고 dictionary값 넣어주면 됨.

--- Bserver/app.py
+++ Bserver/app.py
@@ -1,16 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import requests
+import json
 from templates import Connector
 
 app = Flask(__name__)
 
-@app.route('/B_rasp ')
-def B_rasp(direction_code):
+#direction_code = requests.post('http://localhost:7777','303')
+
+@app.route('/bbb', methods=['POST'])
+def B_rasp():
     try:
-        Connector.serial_connection(direction_code)
+        dataDict = json.loads(request.data)
+        return str(dataDict)
+        #Connector.serial_connection(dataDict["code_num"])
     except:
         HaveError = {'word':'Error!!!!', 'code_num': 555}
-        res = requests.post('CheckingURL', HaveError)
+        res = requests.post('http://localhost:8888/checking', HaveError)
+        return HaveError['word']
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='localhost', port='7777', debug = True)
