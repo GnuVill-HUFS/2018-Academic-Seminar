920a492050a0a5be6e329ea2c78507909cdfb83d
jenny6585 <39253236+jenny6585@users.noreply.github.com>
2018-11-18 21:07:19 +0900

    A 서버 마지막 병합 및 테스트필요

--- Aserver/app.py
+++ Aserver/app.py
@@ -1,24 +1,49 @@
-from flask import Flask,render_template
-import requests
-import json
+from flask import Flask,render_template, redirect, url_for
+import requests, json, webbrowser
 from flask import request
+import os
+from urllib.parse import urljoin
 
 app = Flask(__name__)
 
-@app.route('/aaron', methods=['POST'])
+@app.route('/aaron', methods=['POST','GET'])
 def A_rasp():
-    try:
-        data = json.loads(request.data)
-        res = requests.post('http://localhost:7777/bbb', data = json.dumps(data))
-        return json.dumps(data)
-    except:
-        HaveError = {'word':'Error!!!!', 'code_num': 312}
-        return render_template('woo.html', names=HaveError)
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.data)#푸는것
+            res = requests.post('http://localhost:4444/bbb', data = json.dumps(data))
+            return json.dumps(data)
+        except:
+            HaveError = {'code_num': 312}
+            #file_path = 'woo.html'
+            #file_path = urljoin('file://'.os.path.abspath(file_path))
+            return render_template('woo.html', names=HaveError)
 
-@app.route('/checking',methods=['POST'])
+@app.route('/checking',methods=['POST','GET'])
 def Check():
-    back = request.data
-    return render_template('woo.html', names=back)
+    if request.method == 'POST':
+        back = json.loads(request.data)
+        res = requests.post('http://localhost:3333/save', data=json.dumps(back))
+        #return render_template('woo.html', names = back)
+        return res.text
+
+    elif request.method == 'GET':
+        data = request.args.get('code_num')
+        #word = data['word']
+        return webbrowser.open('http://localhost:3333/save?name=1&code_num='+str(data))
+
+@app.route('/save', methods = ['GET','POST'])
+def save():
+    if request.method == 'POST':
+        back = json.loads(request.data)
+        res = requests.get('http://localhost:3333/checking', params=back)
+        return res.text
+
+    elif request.method=='GET':
+        code_num = request.args.get('code_num')
+        #return str(code_num)
+        return render_template('woo.html', code_num = str(code_num))
+
 
 if __name__ ==  '__main__':
-    app.run(host='localhost',port='8888', debug=True)
+    app.run(host='localhost',port='3333', debug=False)
